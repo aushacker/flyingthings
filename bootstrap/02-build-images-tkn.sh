27bbf80bf6aa07111ba7a08d56c889db1c68cb70
David White <davwhite@davwhite.com>
2023-09-30 21:20:21 -0400

    debug yolo images

--- bootstrap/02-build-images-tkn.sh
+++ bootstrap/02-build-images-tkn.sh
@@ -19,16 +19,12 @@ tkn pipeline start flyingthings-images-pipeline \
   -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
   -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
   -p git-url="https://github.com/davwhite/flyingthings.git" \
-  -p git-revision="main" \
+  -p git-revision="djw" \
   -p YOLO_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
   -p UBI_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
-  -p MINIMAL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
-  -p CUSTOM_NOTEBOOK_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo-notebook:latest" \
-  -p MODEL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/custom-model:latest" \
+  -p MODEL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
   -p BASE_MODEL="yolov5s.pt" \
   -p MINIMAL_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
-  -p MODEL_BUILD_ARGS="--build-arg WEIGHTS=flyingthings.pt --build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
-  -p CUSTOM_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
   -p MINIO_BUCKET="flyingthings" \
   -p PRETRAINED_BUCKET="yolo" \
   -p MINIO_ACCESSKEY="minioadmin" \
